e23418a010942b627dcf61945d5bd51e860ed694
Tu Vo <quangtu2607@gmail.com>
2020-06-26 07:53:52 +0700

    back end demo

---
+++ mainPage/php/_connect_db.php
@@ -0,0 +1,12 @@
+<?php
+
+$connection = mysqli_connect("3306", "root", "Tu1235789", "posts)
+
+if (!$connection) {
+    echo "Error:".mysqli_connect_error();
+    exit;
+}
+
+echo "Connect successful!"
+
+?>
---
+++ mainPage/php/_fetch_data.php
@@ -0,0 +1,6 @@
+<?php
+
+$queury = "SELECT id, title FROM posts ORDER BY id";
+$result = mysqli_query($connection, $query);
+
+?>
