cce84b61aea6e4457253e005080f1abcafc9b7ec
Tu Vo <quangtu2607@gmail.com>
2020-07-06 11:47:28 +0700

    backend added

--- mainPage/js/main.js
+++ mainPage/js/main.js
@@ -1,45 +1,98 @@
+const API_URL = 'http://localhost:4000/posts';
+
+listAllPosts();
+
 function on() {
-  	document.getElementById("overlay").style.display = "flex";
-  	const modal = document.getElementById('overlay');
-  	const b1 = document.getElementById('submit-button');
-  	window.onclick = function(event) {
-  		if (event.target == modal) {
-    	modal.style.display = "none";
-  		}
-  		if (event.target == b1) {
-    	modal.style.display = "none";
-  		}
-	}
-}
+	document.getElementById('overlay').style.display = "flex";
 
-function off() {
-  	document.getElementById("overlay").style.display = "none";
-}
+	const form = document.querySelector('form');
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+		const formData = new FormData(form);
+		const title = formData.get('title');
+		const content = formData.get('content');
 
-function submitButtonClicked() {
-	var tt = document.getElementById('sttt').value;
-	var subject = document.getElementById('stsubject').value;
-	document.getElementById('demo1').innerHTML = tt;
-	document.getElementById('demo2').innerHTML = subject;
-	if (tt != undefined || subject != undefined) {
-		document.getElementById('user-status').style.display = "block";
-	}
-	
-	// var tt = document.getElementById('sttt').value;
-	// var subject = document.getElementById('stsubject').value;
-	// var node = document.createElement("div");
-	// var img = document.createImageBitmap("pic/profile-pic.png");
-	// var text = document.createTextNode("Alo");
-	// node.appendChild(text);
-	// document.getElementById("user-status").appendChild(node);
-}
+		const post = {
+			title,
+			content
+		};
+		
+		fetch(API_URL, {
+			method: 'POST',
+			body: JSON.stringify(post),
+			headers: {
+				'content-type': 'application/json'
+			}
+		}).then(response => response.json())
+			.then(createdPost => {
+				form.reset();
+				listAllPosts();
+			});
+	});
 
-function editStatus() {
 	const modal = document.getElementById('overlay');
-	const edit = document.getElementById('demo4');
-	if (event.target == edit) {
-    	modal.style.display = "flex";
-  	}
+	const b1 = document.getElementById('submit-button');
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+		if (event.target == b1) {
+			modal.style.display = "none";
+		}
+  }
+  return true;
 }
+
+function listAllPosts() {
+	const newsElement = document.querySelector('.user-status');
+	newsElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(posts => {
+            console.log(posts);
+            posts.reverse();
+            posts.forEach(post => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = post.title;
+
+                const contents = document.createElement('p');
+                contents.textContent= post.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(post.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                newsElement.appendChild(div);
+            });
+        })
+}
+
+
+// function off() {
+// 	document.getElementById("overlay").style.display = "none";
+// }
+
+// function submitButtonClicked() {
+// 	var tt = document.getElementById('sttt').value;
+// 	var subject = document.getElementById('stsubject').value;
+// 	document.getElementById('demo1').innerHTML = tt;
+// 	document.getElementById('demo2').innerHTML = subject;
+// 	if (tt != undefined || subject != undefined) {
+// 		document.getElementById('user-status').style.display = "block";
+// 	}
+// }
+
+// function editStatus() {
+// 	const modal = document.getElementById('overlay');
+// 	const edit = document.getElementById('demo4');
+// 	if (event.target == edit) {
+//     	modal.style.display = "flex";
+//   	}
+// }
 
 
---
+++ mainPage/server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+const Filter = require('bad-words');
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+const db = monk('localhost/poststatus');
+const posts = db.get('posts');
+const filter = new Filter();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'OK!'
+    });
+});
+
+app.get('/posts', (req, res) => {
+    posts
+        .find()
+        .then(posts => {
+            res.json(posts);
+        });
+})
+
+function isValidPost(post) {
+    return post.title && post.title.toString().trim() !== '' &&
+        post.content && post.content.toString().trim() !== '';
+}
+
+app.use(rateLimit({
+    windowMs: 30*1000, 
+    max: 1
+}));
+
+app.post('/posts', (req, res) => {
+    if(isValidPost(req.body)) {
+        //insert to db
+        const post = {
+            title: filter.clean(req.body.title.toString()),
+            content: filter.clean(req.body.content.toString()),
+            created: new Date()
+        };
+        
+        posts
+            .insert(post)
+            .then(createdPost => {
+                res.json(createdPost);
+            });
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Hey! Title and Content are required!'
+        });
+    }
+})
+
+app.listen(4000, () => {
+    console.log('Listening on http://localhost:4000');
+});
